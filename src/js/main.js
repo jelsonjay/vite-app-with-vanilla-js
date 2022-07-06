8bcf420cc1fd8fe5c84528d21df942fb60fc90cb
Jelson J <jelson20@outlook.com>
2022-07-06 17:50:59 +0100

    add src folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
-import './style.css';
+import '../css/style.css';
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello World!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
+console.log('Hello World!');
